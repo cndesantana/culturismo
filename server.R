67eb8b5be8e56d276d5cfaa5b37a3f946512501e
cndesantana <charles.desantana@eawag.ch>
2017-10-26 15:38:04 -0300

    Fixing bugs

--- server.R
+++ server.R
@@ -1,60 +1,4 @@
 function(input, output, session) {
-
-  # pkgStream is a reactive expression that represents a stream of
-  # new package download data; up to once a second it may return a
-  # data frame of new downloads since the last update.
-  pkgStream <- packageStream(session)
-
-  # Max age of data (5 minutes)
-  maxAgeSecs <- 60 * 5
-
-  # pkgData is a reactive expression that accumulates previous
-  # values of pkgStream, discarding any that are older than
-  # maxAgeSecs.
-  pkgData <- packageData(pkgStream, maxAgeSecs)
-
-  # dlCount is a reactive expression that keeps track of the total
-  # number of rows that have ever appeared through pkgStream.
-  dlCount <- downloadCount(pkgStream)
-
-  # usrCount is a reactive expression that keeps an approximate
-  # count of all of the unique users that have been seen since the
-  # app started.
-  usrCount <- userCount(pkgStream)
-
-  # Record the time that the session started.
-  startTime <- as.numeric(Sys.time())
-
-  output$rate <- renderValueBox({
-    # The downloadRate is the number of rows in pkgData since
-    # either startTime or maxAgeSecs ago, whichever is later.
-    elapsed <- as.numeric(Sys.time()) - startTime
-    downloadRate <- nrow(pkgData()) / min(maxAgeSecs, elapsed)
-
-    valueBox(
-      value = formatC(downloadRate, digits = 1, format = "f"),
-      subtitle = "Downloads per sec (last 5 min)",
-      icon = icon("area-chart"),
-      color = if (downloadRate >= input$rateThreshold) "yellow" else "aqua"
-    )
-  })
-
-  output$count <- renderValueBox({
-    valueBox(
-      value = dlCount(),
-      subtitle = "Total downloads",
-      icon = icon("download")
-    )
-  })
-
-  output$users <- renderValueBox({
-    valueBox(
-      usrCount(),
-      "Unique users",
-      icon = icon("users")
-    )
-  })
-
   plotReactionsTS = function(){
      url <- input$urlpost
      #     id_pagina <- input$fbid 
@@ -82,7 +26,47 @@ function(input, output, session) {
         coord_flip()    
   }
   
-  output$reactionsPlot <- renderPlot({
+  output$route2booksPlot <- renderPlot({
+     invalidateLater(1*60*1000)
+     url <- input$urlpost
+     #     id_pagina <- input$fbid 
+     id_pagina <- getFBID(url)
+     data <- input$date
+     
+     # command file.path already controls for the OS
+     #     load(paste(workdir,"/fb_oauth",sep=""));
+     load("/home/cdesantana/DataSCOUT/Objectiva/PapoCorreria/dashboard/fb_oauth")     
+     
+     data_inicio <- ymd(as.character(data)) + days(-2);
+     data_final <- ymd(as.character(data)) + days(2);
+     
+     mypage <- getPage(id_pagina, token = fb_oauth, feed=TRUE, since= as.character(data_inicio), until=as.character(data_final))
+     id_post <- mypage$id[which(as.character(mypage$link)%in%url)]
+     
+     post_reactions <- getReactions(id_post, token=fb_oauth)
+     counts <- as.numeric(post_reactions[,-1])
+     
+     reactions_timeseries_filename <- "/home/cdesantana/DataSCOUT/Objectiva/PapoCorreria/dashboard/reactions_per_time.csv"   
+     nloves <- counts[2]; nangries <- counts[6];     
+     if(file.exists(reactions_timeseries_filename)){
+        write.table(data.frame(created_time = Sys.time(), loves = nloves, angries = nangries, is = (nloves - nangries)/(nloves+nangries)), file=reactions_timeseries_filename,sep=",",append=TRUE, col.names = FALSE,row.names=FALSE)
+     }else{
+        write.table(data.frame(created_time = Sys.time(), loves = nloves, angries = nangries, is = (nloves - nangries)/(nloves+nangries)), file=reactions_timeseries_filename,sep=",", col.names = TRUE,row.names=FALSE)        
+     }
+     
+     reactions <- c("Likes","Loves","Haha","Wow","Sad", "Angry")
+     percent <- signif(100*(counts/sum(counts)),1)
+     ggplot() + 
+        geom_bar(stat="identity", aes(x=reactions, y = counts)) + 
+        xlab("Reações") + 
+        ylab("Número de Ocorrências") + 
+        coord_flip()
+     
+  })
+  
+  
+  
+  output$books2routePlot <- renderPlot({
      invalidateLater(1*60*1000)
      url <- input$urlpost
      #     id_pagina <- input$fbid 
